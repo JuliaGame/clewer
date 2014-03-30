7698e801f93724fc8cf93f26be40737802b58e00
Jan Niklas Hasse <jhasse@gmail.com>
2014-03-30 23:27:13 +0200

    Create shader program

--- src/main.jl
+++ src/main.jl
@@ -1,15 +1,26 @@
-const OpenGLver="4.2"
+
+OPENGL_MAJOR = 4
+OPENGL_MINOR = 2
+
+const OpenGLver="$OPENGL_MAJOR.$OPENGL_MINOR"
 import OpenGL
 import GLFW
 
 GLFW.Init()
 
-GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 4)
-GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 2)
+GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, OPENGL_MAJOR)
+GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, OPENGL_MINOR)
 
 window = GLFW.CreateWindow(800, 600, "Hello World", C_NULL, C_NULL)
 
 window->MakeContextCurrent()
+
+vao = 0
+buffer = 0
+fbo = 0
+texture = 0
+shaderProgram = glCreateProgram()
+assert(shaderProgram != 0)
 
 while GLFW.WindowShouldClose(window) == 0
     window->SwapBuffers()
