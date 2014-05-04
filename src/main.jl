06120e056b673aaf0217baece849eec467601f54
Jan Niklas Hasse <jhasse@gmail.com>
2014-05-04 15:39:20 +0200

    Fix usage of GLFW.jl

--- src/main.jl
+++ src/main.jl
@@ -3,7 +3,7 @@ OPENGL_MAJOR = 4
 OPENGL_MINOR = 2
 
 const OpenGLver="$OPENGL_MAJOR.$OPENGL_MINOR"
-import OpenGL
+using OpenGL
 import GLFW
 
 GLFW.Init()
@@ -13,7 +13,7 @@ GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, OPENGL_MINOR)
 
 window = GLFW.CreateWindow(800, 600, "Hello World", C_NULL, C_NULL)
 
-window->MakeContextCurrent()
+GLFW.MakeContextCurrent(window)
 
 vao = 0
 buffer = 0
@@ -23,7 +23,9 @@ shaderProgram = glCreateProgram()
 assert(shaderProgram != 0)
 
 while GLFW.WindowShouldClose(window) == 0
-    window->SwapBuffers()
+    glClearColor(0, 0, 1, 1)
+    glClear(GL_COLOR_BUFFER_BIT)
+    GLFW.SwapBuffers(window)
     GLFW.PollEvents()
 end
 
