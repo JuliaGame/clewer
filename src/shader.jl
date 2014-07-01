349c4aa517043ee77cf796c5f94e5eb829564f11
Jan Niklas Hasse <jhasse@gmail.com>
2014-07-02 00:17:43 +0200

    Don't use deprecated pointer function

--- src/shader.jl
+++ src/shader.jl
@@ -5,7 +5,7 @@ function newShader(filename :: String, shaderType)
     shader = glCreateShader(shaderType)
     assert(shader != 0)
     tmp = Array(Ptr{Uint8}, 1)
-    tmp[1] = pointer(src)
+    tmp[1] = convert(Ptr{Uint8}, src)
     glShaderSource(shader, 1, pointer(tmp), C_NULL)
     glCompileShader(shader)
     status = Array(GLint, 1)
@@ -13,7 +13,8 @@ function newShader(filename :: String, shaderType)
     if status[1] != GL_TRUE
         buffer = Array(Uint8, 512)
         length = Array(Int32, 1)
-        glGetShaderInfoLog(shader, size(buffer, 1), pointer(length), pointer(buffer))
+        glGetShaderInfoLog(shader, size(buffer, 1), convert(Ptr{Int}, length),
+                           convert(Ptr{Int}, buffer))
         error(bytestring(buffer))
     end
     return shader
