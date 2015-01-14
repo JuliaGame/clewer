668873ea25ce4897f0febcd1b438762d432eec27
Jan Niklas Hasse <jhasse@gmail.com>
2015-01-14 13:53:05 +0100

    Adapt to new FreeType version

--- src/freetype.jl
+++ src/freetype.jl
@@ -11,17 +11,20 @@ type Character
     function Character(ch::Char, height, face::FT_Face)
         error = FT_Load_Char(face, ch, FT_LOAD_TARGET_LIGHT | FT_LOAD_RENDER)
         assert(error == 0)
+
+        bitmap = unsafe_load(unsafe_load(face).glyph).bitmap
     end
 end
 
 height = 12
 
 library = Array(FT_Library, 1)
-error = FT_Init_FreeType(pointer(library))
+error = FT_Init_FreeType(library)
 assert(error == 0)
 face = Array(FT_Face, 1)
-error = FT_New_Face(library[1], bytestring("data/fonts/Lato-Lig.otf"), int32(0), pointer(face))
+error = FT_New_Face(library[1], "data/fonts/Lato-Lig.otf", 0, face)
 assert(error == 0)
-FT_Set_Char_Size(face[1], int32(height * 64), int32(height * 64), uint32(96), uint32(96))
+error = FT_Set_Char_Size(face[1], height * 64, height * 64, 96, 96)
+assert(error == 0)
 
 test = Character('a', height, face[1])
