e0bf219b1027dc5885129b6ec776272a61074efc
Jan Niklas Hasse <jhasse@gmail.com>
2015-02-18 16:57:50 +0100

    Replace deprecated syntax

--- src/modelview.jl
+++ src/modelview.jl
@@ -6,16 +6,16 @@ type Modelview
 end
 
 function scale(self::Modelview, x, y=x)
-    self.matrix *= [[x 0 0 0],
-                    [0 y 0 0],
-                    [0 0 1 0],
+    self.matrix *= [[x 0 0 0];
+                    [0 y 0 0];
+                    [0 0 1 0];
                     [0 0 0 1]]
 end
 
 function rotate(self::Modelview, angle)
-    self.matrix *= [[cos(angle) -sin(angle) 0 0],
-                    [sin(angle)  cos(angle) 0 0],
-                    [0           0          1 0],
+    self.matrix *= [[cos(angle) -sin(angle) 0 0];
+                    [sin(angle)  cos(angle) 0 0];
+                    [0           0          1 0];
                     [0           0          0 1]]
 end
 
@@ -26,16 +26,16 @@ function rotate(self::Modelview, angle, direction::Vector{GLfloat})
     x = direction[1]
     y = direction[2]
     z = direction[3]
-    self.matrix *= [[x*x*(1-c)+c     x*y*(1-c)-z*s   x*z*(1-c)+y*s   0],
-                    [y*x*(1-c)+z*s   y*y*(1-c)+c     y*z*(1-c)-x*s   0],
-                    [x*z*(1-c)-y*s   y*z*(1-c)+x*s   z*z*(1-c)+c     0],
+    self.matrix *= [[x*x*(1-c)+c     x*y*(1-c)-z*s   x*z*(1-c)+y*s   0];
+                    [y*x*(1-c)+z*s   y*y*(1-c)+c     y*z*(1-c)-x*s   0];
+                    [x*z*(1-c)-y*s   y*z*(1-c)+x*s   z*z*(1-c)+c     0];
                     [0               0               0               1]]
 end
 
 function translate(self::Modelview, x, y, z=0)
-    self.matrix *= [[1 0 0 x],
-                    [0 1 0 y],
-                    [0 0 1 z],
+    self.matrix *= [[1 0 0 x];
+                    [0 1 0 y];
+                    [0 0 1 z];
                     [0 0 0 1]]
 end
 
